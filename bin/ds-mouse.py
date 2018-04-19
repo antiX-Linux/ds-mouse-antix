02ca9bbb7a29881c88a4d1598ef5a921f028eeec
antiX-Dave <david@daveserver.info>
2018-04-18 23:43:25 -0400

    Update gtk, rearrange layout of sections and add toggle that adds/enables/disables ds-mouse --all in the startup file

--- bin/ds-mouse.py
+++ bin/ds-mouse.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 #Name: ds-mouse.py
-#Depends: python, gtk, xset
+#Depends: python, Gtk, xset
 #Author: Dave (david@daveserver.info)
 #Purpose: Configure mouse on per user basis for a session. This is the gui frontend
 
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, GLib
 import os
 import re
 import gettext
 gettext.install("ds-mouse.py", "/usr/share/locale")
 class Error:
     def __init__(self, error):
-        dlg = gtk.MessageDialog(None, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, message_format=_("There is an error,\nplease rerun and correct the following error!")+"\n\n"+error)
-        dlg.set_title(_("Successfully updated"))
-        dlg.set_keep_above(True) # note: set_transient_for() is ineffective!
+        dlg = Gtk.MessageDialog(None, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, "Error")
+        dlg.format_secondary_text(error)
         dlg.run()
-        dlg.destroy() 
+        dlg.destroy()
        
 class Success:
     def __init__(self, success):
-        dlg = gtk.MessageDialog(None, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, message_format=_("Successfully updated")+":\n\n"+success)
-        dlg.set_title(_("Successfully updated"))
-        dlg.set_keep_above(True) # note: set_transient_for() is ineffective!
-        dlg.run()
-        dlg.destroy() 
+         dlg = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK, "Success")
+         dlg.format_secondary_text(success)
+         dlg.run()
+         dlg.destroy()
        
 class Var: 
     def read(self):        
@@ -31,7 +31,9 @@ class Var:
         var.USER_HOME = os.environ['HOME']
         var.CONF_USER_DIR = var.USER_HOME+"/.desktop-session/"
         var.CONF_USER_FILE = var.CONF_USER_DIR+"mouse.conf"
+        var.CONF_USER_STARTUP = var.CONF_USER_DIR+"startup"
         var.CONF_SYSTEM_FILE = "/etc/desktop-session/mouse.conf"
+        
         
         if not os.path.exists(var.CONF_USER_DIR):
             os.system("mkdir %s" % (var.CONF_USER_DIR))
@@ -49,6 +51,13 @@ class Var:
                     OBJECT=(pieces[1])
                     OBJECT = re.sub(r'\n', '', OBJECT)
                     setattr(var, var.VARIABLE, OBJECT)
+        for line in open(var.CONF_USER_STARTUP):
+            li=line.strip()
+            if re.search(r'ds-mouse' , line):
+                if li.startswith("#"):
+                    var.startstatus = False
+                else:
+                    var.startstatus = True
         
     def write(self, variable, item):
         WRITE_FILE = Var.CONF_USER_FILE+".tmp"
@@ -67,7 +76,7 @@ class Var:
         text.close()        
         os.system("mv %s %s" % ((WRITE_FILE), (READ_FILE)))
 
-class mainWindow():
+class mainWindow(Gtk.Window):
     def apply(self,widget,option):
         if option == 0: #apply button
             acceleration_value = int(self.acceleration.get_value())
@@ -110,110 +119,165 @@ class mainWindow():
             except:
                 os.system("rxvt-unicode -tr -sh 65 -fg white -T 'cursor theme' -e su -c 'update-alternatives --config x-cursor-theme' ")
 		
-    def make_frame(self, text):
-        frame = gtk.Frame(_(text))
+    def make_frame(self, text, pos1, pos2, pos3, pos4):
+        frame = Gtk.Frame(label=_(text))
         frame.set_border_width(10)
-        self.mainbox.pack_start(frame)
+        self.grid.attach(frame, pos1, pos2, pos3, pos4)
         frame.show()
         
-        self.framebox = gtk.VBox()
+        self.framebox = Gtk.VBox()
         frame.add(self.framebox)
         self.framebox.show()
         
     def make_label(self, text):
-        label = gtk.Label()
+        label = Gtk.Label()
         label.set_text(_(text))
-        self.framebox.pack_start(label)
+        self.framebox.pack_start(label, True, True, 0)
         label.show()
         
     def scale_set_default_values(self, scale, option):
-        scale.set_update_policy(gtk.UPDATE_CONTINUOUS)
         scale.set_digits(option)
-        scale.set_value_pos(gtk.POS_TOP)
         scale.set_draw_value(True)
+    
+    def startup_write(self, FROM, TO):
+        TEMP_FILE='/tmp/temp.txt'
+        loop=0
+        text = file((TEMP_FILE), "a")
+        for line in open(Var.CONF_USER_STARTUP, "r").xreadlines():
+            if re.search(r'%s' % (FROM), line):
+               text.write (TO+"\n")
+               loop=1 
+            else:
+               text.write (line) 
+        text.close()
+        if loop == 0 :
+            text = file((TEMP_FILE), "a")
+            text.write ("\n#Enable Mouse Configuration at Startup\nds-mouse --all\n")
+            text.close()
+            
+        os.system("mv %s %s" % ((TEMP_FILE), (Var.CONF_USER_STARTUP)))
+    
+    def toggle_startup(self, widget, object):
+        if self.startup.get_active() == True:
+            try:
+                self.startup_write("ds-mouse", "ds-mouse --all")
+            except:
+                Error(_("Could not turn on startup \n Please edit ~/.desktop-session/startup manually"))
+                self.startup.set_active(False)
+            else:
+                Success(_("Mouse configuration will load on startup"))
+        else:
+            try:
+                self.startup_write("ds-mouse", "#ds-mouse --all")
+            except:
+                Error(_("Could not turn off startup \n Please edit ~/.desktop-session/startup manually"))
+                self.startup.set_active(True)
+            else:
+                Success(_("Mouse configuration will not load on startup"))
 
     def __init__(self):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_title(_("Mouse Options"))
-        window.connect("destroy", lambda w: gtk.main_quit())
+        Gtk.Window.__init__(self)
+        self.set_size_request(300,0)
+        self.set_border_width(10)
+        self.set_title(_("Mouse Options"))
         
-        self.mainbox = gtk.VBox()
-        window.add(self.mainbox)
-        self.mainbox.show()
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
+        self.grid.show()
         
-        self.make_frame(_("Mouse Acceleration"))
+        self.make_frame(_("Mouse Acceleration"),1,1,1,1)
         self.make_label(_("Acceleration (Multiplier)"))
         
-        adj1 = gtk.Adjustment(float(Var.ACCELERATION), 0.0, 17.0, 0.1, 1.0, 1.0 )
-        self.acceleration = gtk.HScale(adj1)
+        adj1 = Gtk.Adjustment(float(Var.ACCELERATION), 0.0, 17.0, 0.1, 1.0, 1.0 )
+        self.acceleration = Gtk.HScale()
+        self.acceleration.set_adjustment(adj1)
         self.acceleration.set_size_request(200, 45)
         self.scale_set_default_values(self.acceleration, 1)
-        self.framebox.pack_start(self.acceleration)
+        self.framebox.pack_start(self.acceleration, False, False, 0)
         self.acceleration.show()
         
         self.make_label(_("Threshold (Pixels)"))
         
-        adj1 = gtk.Adjustment(float(Var.THRESHOLD), 0.0, 101.0, 1.0, 1.0, 1.0 )
-        self.threshold = gtk.HScale(adj1)
+        adj1 = Gtk.Adjustment(float(Var.THRESHOLD), 0.0, 101.0, 1.0, 1.0, 1.0 )
+        self.threshold = Gtk.HScale()
+        self.threshold.set_adjustment(adj1)
         self.threshold.set_size_request(200, 45)
         self.scale_set_default_values(self.threshold, 1)
-        self.framebox.pack_start(self.threshold)
+        self.framebox.pack_start(self.threshold, False, False, 0)
         self.threshold.show()
         
-        reset_motion = gtk.Button(stock=gtk.STOCK_REVERT_TO_SAVED)
+        reset_motion = Gtk.Button(stock=Gtk.STOCK_REVERT_TO_SAVED)
         reset_motion.connect("clicked", self.apply, 1)
-        self.framebox.pack_start(reset_motion)
+        self.framebox.pack_start(reset_motion, False, False, 0)
         reset_motion.show()
         
-        self.make_frame(_("Button Order"))
+        self.make_frame(_("Button Order"),1,2,1,1)
        
-        self.order = gtk.combo_box_new_text()
-        self.order.append_text(_("Right hand layout"))
-        self.order.append_text(_("Left hand layout"))
+        store= Gtk.ListStore(int, str)
+        store.append([0, "Right hand layout"])
+        store.append([1, "Left hand layout"])
+       
+        self.order =  Gtk.ComboBox.new_with_model_and_entry(store)
+        self.order.set_entry_text_column(1)
         self.order.set_active(int(Var.BUTTONORDER))
-        self.framebox.pack_start(self.order)
+        self.framebox.pack_start(self.order, False, False, 17)
         self.order.show()
         
-        self.make_frame(_("Cursor Size"))
+        self.make_frame(_("Cursor Size"),2,1,1,1)
         self.make_label(_("Size (in pixels)"))
         
-        adj1 = gtk.Adjustment(float(Var.SIZE), 10.0, 51.0, 1.0, 1.0, 1.0 )
-        self.size = gtk.HScale(adj1)
+        adj1 = Gtk.Adjustment(float(Var.SIZE), 10.0, 51.0, 1.0, 1.0, 1.0 )
+        self.size = Gtk.HScale()
+        self.size.set_adjustment(adj1)
         self.size.set_size_request(200, 45)
         self.scale_set_default_values(self.size, 0)
-        self.framebox.pack_start(self.size)
+        self.framebox.pack_start(self.size, False, False, 0)
         self.size.show()
         
-        reset_size = gtk.Button(stock=gtk.STOCK_REVERT_TO_SAVED)
+        reset_size = Gtk.Button(stock=Gtk.STOCK_REVERT_TO_SAVED)
         reset_size.connect("clicked", self.apply, 2)
-        self.framebox.pack_start(reset_size)
+        self.framebox.pack_start(reset_size, False, False, 0)
         reset_size.show()
         
-        self.make_frame(_("Cursor Theme"))
-        self.make_label(_("May require logout/login \nto see the changes.\n"))
+        self.make_frame(_("Cursor Theme"), 2,2,1,1)
+        self.make_label(_("May require logout/login \nto see the changes."))
         
-        theme = gtk.Button(_("Change cursor theme"))
+        theme = Gtk.Button(_("Change cursor theme"))
         theme.connect("clicked", self.apply, 3)
-        self.framebox.pack_start(theme)
+        self.framebox.pack_start(theme, False, False, 0)
         theme.show()
+        
+        self.make_frame(_("Startup"),1,3,2,1)
+        self.make_label(_("Enable or Disable mouse configuration on startup\n"))
+        
+        self.startup = Gtk.Switch()
+        self.startup.set_size_request(0,40)
+        self.framebox.pack_start(self.startup, False, False, 0)
+        if Var.startstatus :
+            self.startup.set_active(True)
+        else:
+            self.startup.set_active(False)        
+        self.startup.connect("notify::active", self.toggle_startup)
+        self.startup.show()
         
         #BUTTON BOX
         
-        buttonbox = gtk.HButtonBox()
-        self.mainbox.pack_start(buttonbox)
+        buttonbox = Gtk.HButtonBox()
+        self.grid.attach(buttonbox, 2,4, 1, 1)
         buttonbox.show()
         
-        aply = gtk.Button(stock=gtk.STOCK_APPLY)
+        aply = Gtk.Button(stock=Gtk.STOCK_APPLY)
         aply.connect("clicked", self.apply, 0)
-        buttonbox.pack_start(aply)
+        buttonbox.pack_start(aply, False, False, 0)
         aply.show()
         
-        close = gtk.Button(stock=gtk.STOCK_CLOSE)
-        close.connect("clicked", lambda w: gtk.main_quit())
-        buttonbox.add(close)
+        close = Gtk.Button(stock=Gtk.STOCK_CLOSE)
+        close.connect("clicked", lambda w: Gtk.main_quit())
+        buttonbox.pack_start(close, False,False, 0)
         close.show()
-        window.show()
 
 Var().read()
-mainWindow()
-gtk.main()
+win = mainWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
